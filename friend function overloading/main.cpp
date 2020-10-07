bdbc9193476f0d7b2fe45843ac0f63c2206b77da
vardhan1404 <61473372+vardhan1404@users.noreply.github.com>
2020-10-08 00:04:37 +0530

    7/10

--- friend function overloading/main.cpp
+++ friend function overloading/main.cpp
@@ -4,41 +4,41 @@
 
 */
 
-
-
-
-
-
-
-
 #include <iostream>
 #include<conio.h>
 using namespace std;
 
-class B; // forward declaration of class B
+class B;          // forward declaration of class B
 class A
 {
 private:
     int a;
+
 public:
     void setdata(int x)
     {
         a=x;
     }
-    friend void fun(A,B);
+
+    friend void fun(A,B);         // to prevent error of this line, forward declaration is necessery
 };
+
+
 class B
 {
 private:
     int b;
+
 public:
+        void setdata(int y)
+    {
+        b=y;
+    }
+
     friend void fun(A,B);
-    void setdata(int y)
-                 {
-                     b=y;  }
 
 };
-void fun(A o1, B o2)
+void fun(A o1, B o2)     // defining friend function outside class
 {
     cout<<"sum is "<<o1.a + o2.b;   // this if you want to access private data of more than one class, friend function in use
 
@@ -47,8 +47,10 @@ int main()
 {
     A obj1;
     B obj2;
+
     obj1.setdata(4);
     obj2.setdata(6);
+
     fun(obj1,obj2);
     return 0;
 
