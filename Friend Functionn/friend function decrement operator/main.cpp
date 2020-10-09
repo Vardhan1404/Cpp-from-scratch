69aee01b988911af1ce2dee1e81b7fb41ca4679a
vardhan1404 <61473372+vardhan1404@users.noreply.github.com>
2020-10-09 18:21:53 +0530

    9/10

---
+++ Friend Functionn/friend function decrement operator/main.cpp
@@ -0,0 +1,48 @@
+// If we want to overload binary operator as member function; then pass only one argument.
+// but when overload it as friend func; then the earlier caller operand is now also passed as argument. therefore , pass two arguments.
+
+// in Unary operator, we passed no argument in overloading as member function
+// but in friend func, caller object has to become argument, thus one argument to be passed.
+
+
+// Overloading of unary operator as a friend function
+
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class complex
+{
+private:
+    int a,b;
+
+public:
+    void setdata(int x, int y)
+    {
+        a=x;
+        b=y;
+    }
+
+    void showdata()
+    {
+        cout<<"a = "<<a<<" b = "<<b<<endl; }
+friend complex operator -(complex);
+
+};
+complex operator -(complex X)
+{
+    complex temp;
+    temp.a=-X.a;
+    temp.b=-X.b;
+    return(temp);
+}
+int main()
+{
+    complex c1,c2;
+    c1.setdata(5,8);
+    c2 = -c1;     // c2=operator-(c1)
+    c1.showdata();
+    c2.showdata();
+    return 0;
+}
