5c095bd40b14106947aae16df028bff995038f81
vardhan1404 <61473372+vardhan1404@users.noreply.github.com>
2020-10-13 23:58:22 +0530

    13/10

--- Virtual function/main.cpp
+++ Virtual function/main.cpp
@@ -37,3 +37,19 @@ int main()
 // With VIRTUAL , this returns f1 of class B, as it looks for the address contained in pointer , which is of object of class B.
     return 0;
 }
+
+
+/* VIRTUAL FUNCTION WORKING CONCEPT
+
+     RULES
+- Any class where atleast one virtual function exists, in that class the compiler declares the variable as the member in that class.
+- It says that if any class contains atleast one virtual function then object created of that class must contains/exists vptr pointer.
+- No separate vptr pointer for child class if one is already in parent class.
+- Any class where atleast one virtual function exists, there must exists a vtable (static array) - which consists of pointers containing address of virtual functions.
+- for descedent classes, separate individual vtable is there
+- vtable: the static array, does not depend upon objects.
+- vptr contains address of static array, if A class object is made, then vptr contains class A vtable address.
+
+
+*/
+
