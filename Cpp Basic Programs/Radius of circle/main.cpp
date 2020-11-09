697129feebc9cf2f25a2f6ef8aae600658851c21
vardhan1404 <61473372+vardhan1404@users.noreply.github.com>
2020-11-09 23:18:47 +0530

    9/11

---
+++ Cpp Basic Programs/Radius of circle/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    float a;
+    cout<<"Enter radius of circle: "<<endl;
+    cin>>r;
+    a = 3.14*r*r;
+    cout<<"The Area of circle is : "<<a<<endl;
+    return 0;
+}
