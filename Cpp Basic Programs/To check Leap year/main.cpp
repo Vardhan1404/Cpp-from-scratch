178cf64890b4c70157be0b09ca93be3f97c3e7ab
vardhan1404 <61473372+vardhan1404@users.noreply.github.com>
2020-11-09 23:34:37 +0530

    9/11

---
+++ Cpp Basic Programs/To check Leap year/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int year;
+    cout<<"Enter year: "<<endl;
+    cin>>year;
+
+    if(year%400==0 || year%100!=0 && year%4==0)
+        cout<<"It is a LEAP year ! "<<endl;
+    else
+        cout<<"No LEAP year "<<endl;
+    return 0;
+}
